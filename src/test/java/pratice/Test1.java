077f6268dfdba2f2d7a1493e330f4157856d0490
Admin <Admin@DESKTOP-N0V0TF5>
2022-01-24 17:15:11 +0530

    comit and push

---
+++ src/test/java/pratice/Test1.java
@@ -0,0 +1,14 @@
+package pratice;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	public class PraticeGetHub {
+		
+		@Test
+		public void praticeGitHub() {
+			System.out.println("Hi there this is Nagarjun Frist push");
+		}
+	}
+
+}
